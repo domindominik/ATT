562b32ed449d01b285a136d77c4c68826bf98c6e
dominik <dominwro@gmail.com>
2019-07-12 23:20:24 +0200

    Add Block class.

--- src/main/java/Block.java
+++ src/main/java/Block.java
@@ -1,4 +1,14 @@
 public class Block
 {
+    public String hash;
+    public String previousHash;
+    private String data;
+    private long timeStamp;
 
+    public Block(String previousHash, String data)
+    {
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timeStamp = System.currentTimeMillis();
+    }
 }
